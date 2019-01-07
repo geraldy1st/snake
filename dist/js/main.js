02ee55f39a52509553a6c1ee04e474d3429a9d49
geraldy1st <geraldy.leondas@gmail.com>
2019-01-07 01:10:49 +0100

    snake draw

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,49 @@
 window.onload = function() {
-  var canvas = document.createElement("canvas");
-  canvas.width = 900;
-  canvas.height = 600;
-  canvas.style.border = "1px solid";
-  document.body.appendChild(canvas);
+  var canvasWidth = 900;
+  var canvasHeight = 600;
+  var blockSize = 30;
+  var ctx;
+  var delay = 100;
+  var snakee;
 
-  var ctx = canvas.getContext("2d");
-  ctx.fillStyle = "#ff0000";
-  ctx.fillRect(30, 30, 100, 50);
+  init();
+
+  function init() {
+    var canvas = document.createElement("canvas");
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+    canvas.style.border = "1px solid";
+    document.body.appendChild(canvas);
+    ctx = canvas.getContext("2d");
+    snakee = new Snake([[6, 4], [5, 4], [4, 4]]);
+    refreshCanvas();
+  }
+  function refreshCanvas() {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    snakee.advance();
+    snakee.draw();
+    setTimeout(refreshCanvas, delay);
+  }
+  function drawBlock(ctx, position) {
+    var x = position[0] * blockSize;
+    var y = position[1] * blockSize;
+    ctx.fillRect(x, y, blockSize, blockSize);
+  }
+  function Snake(body) {
+    this.body = body;
+    this.draw = function() {
+      ctx.save();
+      ctx.fillStyle = "#ff0000";
+      for (var i = 0; i < this.body.length; i++) {
+        drawBlock(ctx, this.body[i]);
+      }
+      ctx.restore();
+    };
+    this.advance = function() {
+      var nextPosition = this.body[0].slice();
+      nextPosition[0] += 1;
+      this.body.unshift(nextPosition);
+      this.body.pop();
+    };
+  }
 };
